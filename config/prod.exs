7c3faf0db4a972d428dbf2a6ede80b57a9d0f5e4
Steve Wagner <ciroque@live.com>
2017-05-13 16:38:08 -0700

    commenting out the url since I have no idea what it does. Want to see if that corrects the problem with this starting up on the Pi

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :firewerx, Firewerx.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "safat.wagner-x.net", port: 80],
+#  url: [host: "safat.wagner-x.net", port: 80],       ## Don't understand this yet...
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
