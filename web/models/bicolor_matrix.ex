e3e46aa18e37d92a99023fefc92ea2b44ae14ee3
Steve Wagner <ciroque@live.com>
2017-05-20 01:02:33 -0700

    reestablishing the better rendering implementation.

--- web/models/bicolor_matrix.ex
+++ web/models/bicolor_matrix.ex
@@ -8,8 +8,10 @@ defmodule BicolorMatrix do
   @yellow 3
 
   def initialize() do
-    { :ok, pid } = I2C.start_link("i2c-1", 0x70)
-    pid
+    pid = start()
+    I2C.write(pid, <<0x21, 0x00>>)
+    I2C.write(pid, <<0x81, 0x00>>)
+    release(pid)
   end
 
   def release(pid) do
@@ -17,7 +19,9 @@ defmodule BicolorMatrix do
   end
 
   def clear() do
-    Enum.each(0..15, fn x -> write(<<x, 0x00>>) end)
+    pid = start()
+    Enum.each(0..15, fn x -> write(pid, <<x, 0x00>>) end)
+    release(pid)
   end
 
   def draw_index(index) do
@@ -30,80 +34,107 @@ defmodule BicolorMatrix do
     end
   end
 
-  defp write(data) do
-    pid = initialize()
+  def set_brightness(brightness) do
+    unless brightness < 0 || brightness > 15 do
+      pid = start()
+      I2C.write(pid, <<0xFFFF, brightness>>)
+      release(pid)
+    end
+  end
+
+  defp start() do
+    { :ok, pid } = I2C.start_link("i2c-1", 0x70)
+    pid
+  end
+
+  defp write(pid, data) do
     I2C.write(pid, data)
-    release(pid)
   end
 
   def draw_target() do
-    write(<<0x00, 0xFF>>)
+    pid = start()
 
-    write(<<0x02, 0xFF>>)
-    write(<<0x03, 0x7E>>)
+    write(pid, <<0x00, 0xFF>>)
 
-    write(<<0x04, 0xFF>>)
-    write(<<0x05, 0x7E>>)
+    write(pid, <<0x02, 0xFF>>)
+    write(pid, <<0x03, 0x7E>>)
 
-    write(<<0x06, 0xE7>>)
-    write(<<0x07, 0x7E>>)
+    write(pid, <<0x04, 0xFF>>)
+    write(pid, <<0x05, 0x7E>>)
 
-    write(<<0x08, 0xE7>>)
-    write(<<0x09, 0x7E>>)
+    write(pid, <<0x06, 0xE7>>)
+    write(pid, <<0x07, 0x7E>>)
+
+    write(pid, <<0x08, 0xE7>>)
+    write(pid, <<0x09, 0x7E>>)
     
-    write(<<0x0A, 0xFF>>)
-    write(<<0x0B, 0x7E>>)
+    write(pid, <<0x0A, 0xFF>>)
+    write(pid, <<0x0B, 0x7E>>)
     
-    write(<<0x0C, 0xFF>>)
-    write(<<0x0D, 0x7E>>)
+    write(pid, <<0x0C, 0xFF>>)
+    write(pid, <<0x0D, 0x7E>>)
 
-    write(<<0x0E, 0xFF>>)
+    write(pid, <<0x0E, 0xFF>>)
+
+    release(pid)
   end
 
   def draw_green_x() do
-    write(<<0x01, 0xFF>>)
+    pid = start()
+
+    write(pid, <<0x01, 0xFF>>)
     
-    write(<<0x02, 0x7E>>)
-    write(<<0x03, 0xBD>>)
+    write(pid, <<0x02, 0x7E>>)
+    write(pid, <<0x03, 0xBD>>)
 
-    write(<<0x04, 0x7E>>)
-    write(<<0x05, 0xDB>>)
+    write(pid, <<0x04, 0x7E>>)
+    write(pid, <<0x05, 0xDB>>)
 
-    write(<<0x06, 0x7E>>)
-    write(<<0x07, 0xE7>>)
+    write(pid, <<0x06, 0x7E>>)
+    write(pid, <<0x07, 0xE7>>)
 
-    write(<<0x08, 0x7E>>)
-    write(<<0x09, 0xE7>>)
+    write(pid, <<0x08, 0x7E>>)
+    write(pid, <<0x09, 0xE7>>)
    
-    write(<<0x0A, 0x7E>>)
-    write(<<0x0B, 0xDB>>)
+    write(pid, <<0x0A, 0x7E>>)
+    write(pid, <<0x0B, 0xDB>>)
      
-    write(<<0x0C, 0x7E>>)
-    write(<<0x0D, 0xBD>>)
+    write(pid, <<0x0C, 0x7E>>)
+    write(pid, <<0x0D, 0xBD>>)
 
-    write(<<0x0F, 0xFF>>)
+    write(pid, <<0x0F, 0xFF>>)
+
+    release(pid)
   end
 
   def draw_red_dot() do
-    write(<< 0x01, 0x18 >>)
-    write(<< 0x03, 0x3C >>)
-    write(<< 0x05, 0x7E >>)
-    write(<< 0x07, 0xFF >>)
-    write(<< 0x09, 0xFF >>)
-    write(<< 0x0B, 0x7E >>)
-    write(<< 0x0D, 0x3C >>)
-    write(<< 0x0F, 0x18 >>)
+    pid = start()
+
+    write(pid, <<0x01, 0x18>>)
+    write(pid, <<0x03, 0x3C>>)
+    write(pid, <<0x05, 0x7E>>)
+    write(pid, <<0x07, 0xFF>>)
+    write(pid, <<0x09, 0xFF>>)
+    write(pid, <<0x0B, 0x7E>>)
+    write(pid, <<0x0D, 0x3C>>)
+    write(pid, <<0x0F, 0x18>>)
+
+    release(pid)
   end
 
   def draw_green_dot() do
-    write(<< 0x00, 0x18 >>)
-    write(<< 0x02, 0x3C >>)
-    write(<< 0x04, 0x7E >>)
-    write(<< 0x06, 0xFF >>)
-    write(<< 0x08, 0xFF >>)
-    write(<< 0x0A, 0x7E >>)
-    write(<< 0x0C, 0x3C >>)
-    write(<< 0x0E, 0x18 >>)
+    pid = start()
+
+    write(pid, <<0x00, 0x18>>)
+    write(pid, <<0x02, 0x3C>>)
+    write(pid, <<0x04, 0x7E>>)
+    write(pid, <<0x06, 0xFF>>)
+    write(pid, <<0x08, 0xFF>>)
+    write(pid, <<0x0A, 0x7E>>)
+    write(pid, <<0x0C, 0x3C>>)
+    write(pid, <<0x0E, 0x18>>)
+
+    release(pid)
   end
 
   def draw_yellow_dot() do
@@ -112,29 +143,33 @@ defmodule BicolorMatrix do
   end
 
   def draw_christmas_checkers() do
-    write(<< 0x00, 0xAA >>)
-    write(<< 0x01, 0x55 >>)
+    pid = start()
+
+    write(pid, <<0x00, 0xAA>>)
+    write(pid, <<0x01, 0x55>>)
     
-    write(<< 0x03, 0xAA >>)
-    write(<< 0x02, 0x55 >>)
+    write(pid, <<0x03, 0xAA>>)
+    write(pid, <<0x02, 0x55>>)
 
-    write(<< 0x04, 0xAA >>)
-    write(<< 0x05, 0x55 >>)
+    write(pid, <<0x04, 0xAA>>)
+    write(pid, <<0x05, 0x55>>)
 
-    write(<< 0x07, 0xAA >>)
-    write(<< 0x06, 0x55 >>)
+    write(pid, <<0x07, 0xAA>>)
+    write(pid, <<0x06, 0x55>>)
 
-    write(<< 0x08, 0xAA >>)
-    write(<< 0x09, 0x55 >>)
+    write(pid, <<0x08, 0xAA>>)
+    write(pid, <<0x09, 0x55>>)
 
-    write(<< 0x0B, 0xAA >>)
-    write(<< 0x0A, 0x55 >>)
+    write(pid, <<0x0B, 0xAA>>)
+    write(pid, <<0x0A, 0x55>>)
 
-    write(<< 0x0C, 0xAA >>)
-    write(<< 0x0D, 0x55 >>)
+    write(pid, <<0x0C, 0xAA>>)
+    write(pid, <<0x0D, 0x55>>)
 
-    write(<< 0x0F, 0xAA >>)
-    write(<< 0x0E, 0x55 >>)
+    write(pid, <<0x0F, 0xAA>>)
+    write(pid, <<0x0E, 0x55>>)
+
+    release(pid)
   end
 
   def draw_random() do
